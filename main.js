071910f1d9be8b65104792919681672386d46e56
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-03-30 11:34:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-//https://teachablemachine.withgoogle.com/models/THE0KuYGb/
-function startdancing(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/GN8Mc70Ztw/model.json',modelloaded);
+var prediction1="";
+var prediction2="";
+
+console.log("ml5 version="+ml5.version);
+var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/797nNedup/model.json',model_loaded);
+function model_loaded(){
+    console.log("model is loaded");
 }
 
-function modelloaded(){
-    classifier.classify(gotresult);
+Webcam.set({
+    width:350,
+    heigth:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot(){
+    Webcam.snap(
+        function (data_uri){
+            document.getElementById("result").innerHTML='<img id="captureimage" src="'+data_uri+'">';
+        }
+    );
 }
 
-function gotresult(error,results){
-    if (error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        document.getElementById("soundname").innerHTML="I can hear- "+results[0].label;
-        if(results[0].label=="Dog"){
-            document.getElementById("image").src="dog.jfif";
-        }
-        if(results[0].label=="Cat"){
-            document.getElementById("image").src="cat.jfif";
-        }
-    }
+function speak(){
+    var synth=window.speechSynthesis;
+    speakdata1="the first preidition is "+prediction1;
+    speakdata2="and the second preidition is "+prediction2;
+    var utterThis=new SpeechSynthesisUtterance(speakdata1+speakdata2);
+    synth.speak(utterThis)
 }
