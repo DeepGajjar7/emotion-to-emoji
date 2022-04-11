0c641ef071a21753016f7a5dabfc724949b8c608
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-04-11 11:25:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,40 @@ function speak(){
     var utterThis=new SpeechSynthesisUtterance(speakdata1+speakdata2);
     synth.speak(utterThis)
 }
+
+function check(){
+    img=document.getElementById("captureimage");
+    classifier.classify(img,gotresults)
+}
+
+function gotresults(error,results){
+    if (error){
+        console.error(error)
+    }
+    else {
+        console.log(results);
+        prediction1=results[0].label;
+        prediction2=results[1].label;
+        document.getElementById("result_emotion_name").innerHTML=prediction1;
+        document.getElementById("result_emotion_name1").innerHTML=prediction2;
+        speak();
+        if (prediction1=="Happy"){
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+        }
+        if (prediction1=="Sad"){
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+        }
+        if (prediction1=="Angry"){
+            document.getElementById("update_emoji").innerHTML="&#128545;";
+        }
+        if (prediction2=="Happy"){
+            document.getElementById("update_emoji1").innerHTML="&#128522;";
+        }
+        if (prediction2=="Sad"){
+            document.getElementById("update_emoji1").innerHTML="&#128532;";
+        }
+        if (prediction2=="Angry"){
+            document.getElementById("update_emoji1").innerHTML="&#128545;";
+        }
+    }
+}
